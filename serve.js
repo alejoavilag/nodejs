8c93ff670a1dbb1422313e7937a7ae85fd6173c4
Alejo Avila <alejandroavilaguerrero@gmail.com>
2020-09-29 11:17:58 -0500

    [CREATE] branch circleci and cretae config.yml

--- serve.js
+++ serve.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+const cors = require('cors')
 const router = require('./componets/articulos/routes')
 
 const app = express()
+app.use(cors())
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended: false}))
 app.use('/articles', router)
+
+
 
 
 //app.use('/app', express.static('public'))
